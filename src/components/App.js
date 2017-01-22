39f917f2591f5e4510e0802d0cb961fd4526a604
Helgi Helgason <HelgiHel@gmail.com>
2017-01-22 15:53:13 +0000

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ChannelList } from './ChannelList';
+import ChannelList from './ChannelList';
+import Router from './Router';
 
 const App = () => {
   return (
-    <ChannelList />
+
+      <Router />
+
   );
 };
 
